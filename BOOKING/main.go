8a80cb6fef32df25a51529487ca08a15d45ca18a
Abdulhafiz Oladimeji Dada <158800417+Haaffiiizzz@users.noreply.github.com>
2024-08-03 18:16:02 +0000

    helper functions

--- BOOKING/main.go
+++ BOOKING/main.go
@@ -59,14 +59,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-func validateInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicket := userTickets > 0 && userTickets <= ticketsLeft
-
-	return isValidName, isValidEmail, isValidTicket
-}
-
 func getInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
--- BOOKING/main.py
+++
@@ -1,3 +0,0 @@
-lest = [1, 2, 3]
-for i, j in enumerate(lest):
-    print(i, j)
