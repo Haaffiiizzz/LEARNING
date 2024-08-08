fe10eb78f6a9b862307e3d90b0211f38b4a84ec0
Abdulhafiz Oladimeji Dada <158800417+Haaffiiizzz@users.noreply.github.com>
2024-08-08 03:47:50 +0000

    guess count

--- BOOKING/guess.go
+++ BOOKING/guess.go
@@ -10,14 +10,15 @@ func main() {
 	fmt.Println("Welcome to this CLI guessing game. Goodluck!")
 	rand.Seed(time.Now().UnixNano())
 
-    randomNumber := rand.Intn(11)
+	randomNumber := rand.Intn(11)
 
 	var guess int
-	fmt.Println("\nType in your guess:")
+	fmt.Println("\nType in your guess between 0 and 10:")
 
 	fmt.Scan(&guess)
-
+	guessCount := 0
 	for guess != randomNumber {
+		guessCount = guessCount + 1
 		if guess < randomNumber {
 			fmt.Println("Guess is lower than the number. Try again!")
 			fmt.Scan(&guess)
@@ -28,8 +29,6 @@ func main() {
 
 	}
 
-	fmt.Println("Congrats, you got it right!")
-
-
+	fmt.Printf("Congrats, you got it right with %v guesses!", guessCount)
 
 }
