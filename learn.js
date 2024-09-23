44e341decfaba395b0126ea1bbdadad189c06a49
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-09-23 14:13:34 -0500

    switch case statement

--- learn.js
+++ learn.js
@@ -1,3 +1,27 @@
-let newName = 1
-newName++
-console.log(newName)
+
+let myList = [1, 2, 3, 4, 5, 6]
+
+let randomIndex = Math.floor(Math.random() * myList.length)
+
+switch(randomIndex) {
+    case 1:
+        console.log(1)
+        break;
+    case 2:
+        console.log(2)
+        break;
+    case 3:
+        console.log(3)
+        break;
+    case 4:
+        console.log(4)
+        break;
+    case 5:
+        console.log(5)
+        break;
+    case 6:
+        console.log(6)
+        break;
+
+    
+}
