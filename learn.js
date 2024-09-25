1f9b94f2fd8255439ffff8af030c41cc73630c03
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-09-25 17:29:06 -0500

    func

--- learn.js
+++ learn.js
@@ -1,27 +1,5 @@
-
-let myList = [1, 2, 3, 4, 5, 6]
-
-let randomIndex = Math.floor(Math.random() * myList.length)
-
-switch(randomIndex) {
-    case 1:
-        console.log(1)
-        break;
-    case 2:
-        console.log(2)
-        break;
-    case 3:
-        console.log(3)
-        break;
-    case 4:
-        console.log(4)
-        break;
-    case 5:
-        console.log(5)
-        break;
-    case 6:
-        console.log(6)
-        break;
-
-    
+function printHello(strr) {
+    console.log(strr)
 }
+
+printHello("hrllo")
