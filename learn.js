929ba32bd33b7b6dd5f98e1ca2f6d9423bb073c0
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-10-25 15:20:06 -0500

    mapping

--- learn.js
+++ learn.js
@@ -1,5 +1,5 @@
-function printHello(strr) {
-    console.log(strr)
-}
 
-printHello("helloworld")
+let oldList = [5, 4, 3]
+console.log(oldList)
+newList = oldList.map(mult => mult * 2 );
+console.log(newList)
