b65ef0533b506f4926b991e25fece2636e62679b
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-12-17 22:54:58 -0600

    .

--- learn.js
+++ learn.js
@@ -3,19 +3,13 @@
 // newString = newString.replaceAll("g", "b")
 // console.log(newString.toUpperCase())
 
-let newArray = [1, 2]
-console.log(newArray[1])
+for (let i = 0; i < 1000000; i++){
+    if (i%2 == 0){
+        console.log("Even")
+    }else{
+        console.log("False")
+    }
 
-let newDict = {"hello": "hi"}
-console.log(newDict)
-
-newArray.forEach(element => {
-    console.log(element * 2)
-});
-
-for (let i = 0; i<10; i++) {
-    console.log(i)
 }
-console.log(newArray.includes(1))
 
 // push is to append. shift is to pop(0) unshift is to insert(0 )
